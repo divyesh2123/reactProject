e49c90de261bd25d45dce3543e5cf516eb207810
lenovo <lenovo@LAPTOP-BLNTEBH7>
2022-09-09 17:51:15 +0530

    th

---
+++ src/CustComLink.js
@@ -0,0 +1,39 @@
+import React from 'react'
+
+import {
+  Routes,
+  Route,
+  Outlet,
+  Link,
+  useMatch,
+  useResolvedPath,
+} from "react-router-dom";
+export default function CustComLink({
+     children,
+     to,
+     ...props }) {
+
+    let resolved = useResolvedPath(to);
+    let match = useMatch({ path: resolved.pathname });
+   
+    console.log(match);
+
+   
+
+
+  return (
+    <div>
+<Link
+        style={{
+             textDecoration: match ? "underline" : "none" }}
+        to={to}
+        {...props}
+        
+      >
+        {children}
+      </Link>
+      {match && " (active)"}
+
+    </div>
+  )
+}
--- src/MyHoc.js
+++ src/MyHoc.js
@@ -1,8 +1,9 @@
 import React from "react";
 import WelFooter from "./WelFooter";
+import CustComLink from "./CustComLink";
 import WelHeader from "./WelHeader";
 import {
-  BrowserRouter, Route,Routes,Link } from 'react-router-dom';
+  BrowserRouter, Route,Routes,Link,NavLink } from 'react-router-dom';
 const MyHoc = (Component) => ({...props}) => (
     <div>
       
@@ -13,8 +14,24 @@ const MyHoc = (Component) => ({...props}) => (
         <Link to="/myabout">MyAbout</Link>
         <Link to="/contact">contact</Link>
         <Link to="/login">login</Link>
+        <Link to="/invoices">Invoice</Link>
 
+        <NavLink
+            to="/contact"
+            style={({ isActive }) =>
+              isActive
+                ? {
+                    color: '#fff',
+                    background: '#7600dc',
+                  }
+                : { color: '#545e6f', background: 'red' }
+            }
+          >ContactTest
+            </NavLink>
+            
 
+            <CustComLink to='/contact'>Contact</CustComLink>
+          
       <div>
         <Component {...props}/>
 
